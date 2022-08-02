9c1734b7e0306c6787e8476b91091687a2aa3f78
Salim Azharhoussen <azharhoussen.salim@gmail.com>
2022-08-02 13:24:13 +0200

    donation event

--- contracts/DonationContract.sol
+++ contracts/DonationContract.sol
@@ -2,19 +2,27 @@
 pragma solidity ^0.8.15;
 
 contract DonationContract {
+
+    event Donation(
+        address indexed to,
+        address indexed from, 
+        uint amount
+    );
+
     address payable owner;
-    
+
     constructor() {
         owner = payable(msg.sender);
     }
 
-    function donate() public payable {
+    function donate(address payable to_charity) public payable {
         require(msg.value > 0, 'The donation amount has to be greater than 0');
-        owner.transfer(msg.value);
+        to_charity.transfer(msg.value);
+        emit Donation(to_charity,msg.sender, msg.value);
     }
 
-    function withdraw() public {
+   /* function withdraw() public {
         require(owner==msg.sender, 'Available only to the to the user that deployed the contract');
         owner.transfer(address(this).balance);
-    }
+    }*/
 }
